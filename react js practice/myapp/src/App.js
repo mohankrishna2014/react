ab1adc49fd3dba6239a7bca4ebeabd04b2fb5c59
mohankrishna2014 <113678440+mohankrishna2014@users.noreply.github.com>
2024-10-17 12:38:38 +0530

    Add files via upload

---
+++ react js practice/myapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello World ..! </h1>
+      <button class = 'btn btn-success'> Start Coding </button>
+    </div>
+  );
+}
+
+export default App;
